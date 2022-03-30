97bb6815e7655d7e50b56ebf9eca6b323d6af9db
Sara894 <t48250klo@gmail.com>
2022-03-30 14:25:49 +0300

    Router

--- public/index.php
+++ public/index.php
@@ -1,8 +1,14 @@
 <?php
 
-$query =  $_SERVER['QUERY_STRING'];
+$query =  rtrim($_SERVER['QUERY_STRING'],'/');
 require '../vendor/core/Router.php';
 require '../vendor/libs/functions.php';
 
 Router::add('posts/add',['controller'=>'Posts','action'=>'add']);
 debug(Router::getRoutes());
+
+if(Router::matchRoute($query)){
+    debug(Route::getRoute());
+}else{
+    echo '404';
+}
