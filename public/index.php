c5c80cdf1c161ed825a6d24035acd0418a0974c9
Sara894 <t48250klo@gmail.com>
2022-04-03 21:19:29 +0300

    автозагрузка

--- public/index.php
+++ public/index.php
@@ -1,9 +1,21 @@
 <?php
+define ('WWW',__DIR__);
+define ('CORE',dirname(__DIR__).'/vendor/core');
+define ('ROOT',dirname(__DIR__));
+define ('APP',dirname(__DIR__).'/app');
 require '../vendor/core/Router.php';
 require '../vendor/libs/functions.php';
-require '../app/controllers/Main.php';
+/* require '../app/controllers/Main.php';
 require '../app/controllers/Posts.php';
-require '../app/controllers/PostsNew.php';
+require '../app/controllers/PostsNew.php'; */
+
+spl_autoload_register(function($class){
+    $file = APP."/controllers/$class.php";
+    if(file_exists($file))
+    {
+        require_once $file;
+    }
+} );
 $query =  rtrim($_SERVER['QUERY_STRING'],'/');
 Router::add('^index.php$',['controller'=>'Main','action'=>'index']);
 Router::add('^index.php&(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
