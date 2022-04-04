b36947e0b8a440fe60f4193b6ee917aec019e0b2
Sara894 <t48250klo@gmail.com>
2022-04-04 22:56:10 +0300

    base Controller

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -4,4 +4,8 @@ class Posts{
     public function indexAction(){
         echo "Posts::indexAction";
     }
+
+    public function testAction(){
+        echo "Posts::testAction";
+    }
 }
---
+++ vendor/core/base/Controller.php
@@ -0,0 +1,10 @@
+<?
+namespace vendor\core\base;
+
+abstract class Controller{
+    public $route = [];
+
+    public function __construct($route){
+        $this->$route = $route;
+    }
+}
