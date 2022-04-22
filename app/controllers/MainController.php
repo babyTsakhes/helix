73899e4e2c66b6b23191c1f76a0812518fc80faa
Sara894 <t48250klo@gmail.com>
2022-04-22 20:19:33 +0300

    Cache

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 use app\models\Main;
 use classes\Cache;
 use vendor\core\Registry;
+use vendor\core\App;
 
 
 class MainController extends AppController{
@@ -11,13 +12,17 @@ class MainController extends AppController{
     public $layout = 'main';
 
     public function indexAction(){
-        \vendor\core\App::$app->getList();
+        //App::$app->getList();
         $model = new Main;
         $posts = \R::findAll('posts');
+        App::$app->cache->set('posts',$posts,3600*24);
+        echo date('Y-m-d H:i:s', time());
+        echo '<br>';
+        echo date('Y-m-d H:i:s',1650733906);
         $menu = $this->menu;
         $title  = "POSTS";
         $this->setMeta('Главная страница111','mainpage ahahah ','ключевые слова aliexpress mvideo');
-        $this->test();
+       // $this->test();
         $meta = $this->meta;
         //debug($categories);
         $this->set(compact('posts','menu','meta'));
--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ define ('CORE',dirname(__DIR__).'/vendor/core');
 define ('ROOT',dirname(__DIR__));
 define ('LIBS',dirname(__DIR__).'/vendor/libs');
 define ('APP',dirname(__DIR__).'/app');
+define ('CACHE',dirname(__DIR__).'/tmp/cache');
 define('LAYOUT','default');
 //require '../vendor/core/Router.php';
 require '../vendor/libs/functions.php';
