f8f4089dac7eaf0d40624048b1b38899b3d6a371
Sara894 <t48250klo@gmail.com>
2022-04-28 16:43:36 +0300

    шаблон меню

--- public/menu/my_menu.php
+++ public/menu/my_menu.php
@@ -1,4 +1,4 @@
-<li class="test">
+<li class="test" style="background-color:coral; color:brown">
     <a href="?id=<?=$id?>"><?=$category['title']?></a>
     <?if(isset($category['childs'])):?>
         <ul class="test">
