700f06782c4ba0852dfca17fb66f76ba58e204de
Sara894 <t48250klo@gmail.com>
2022-05-06 10:38:24 +0300

    вид для авторизации

--- app/views/User/login.php
+++ app/views/User/login.php
@@ -1,24 +1,17 @@
- <div id="login" class="animate form">
-            <form  action="mysuperscript.php" autocomplete="on">
-                <h1>Log in</h1>
-                <p>
-                    <label for="username" class="uname" data-icon="u" > Your email or username </label>
-                    <input id="username" name="username" required="required" type="text" placeholder="myusername or mymail@mail.com"/>
-                </p>
-                <p>
-                    <label for="password" class="youpasswd" data-icon="p"> Your password </label>
-                    <input id="password" name="password" required="required" type="password" placeholder="eg. X8df!90EO" />
-                </p>
-                <p class="keeplogin">
-                    <input type="checkbox" name="loginkeeping" id="loginkeeping" value="loginkeeping" />
-                    <label for="loginkeeping">Keep me logged in</label>
-                </p>
-                <p class="login button">
-                    <input type="submit" value="Login" />
-                </p>
-                <p class="change_link">
-                    Not a member yet ?
-                    <a href="#tosubscribe" class="to_subscribe">Join us</a>
-                </p>
-            </form>
-        </div>
+<h2>Log In</h2>
+<div class="row ">
+    <div class = "col-md-6">
+        <form method="post" action = "/user/login">
+        <div class="form-group">
+                <label for="login">Login</label>
+                <input type="text" name="login" class="form-control" id="login" placeholder="Login" value="<?=isset($_SESSION['formData']['login']) ? h($_SESSION['formData']['login']) : '' ?>">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" class="form-control" id="password" placeholder="Password">
+            </div>
+        </form>
+
+    </div>
+
+</div>
