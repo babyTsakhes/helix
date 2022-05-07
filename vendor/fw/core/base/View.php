c6845f869fd8799208912b8a43c8ee7d20e992c4
Sara894 <t48250klo@gmail.com>
2022-05-07 21:22:39 +0300

    форматирование

--- vendor/fw/core/base/View.php
+++ vendor/fw/core/base/View.php
@@ -1,7 +1,9 @@
 <?
 
 namespace fw\core\base;
-class View{
+
+class View
+{
     /**
      * текущий маршрут и параметры(controller,action,alias)
      * @var array
@@ -20,54 +22,57 @@ class View{
 
     public $scripts = [];
 
-    public static $meta = ['title' => '', 'desc'=> '','keywords'=>''];
+    public static $meta = ['title' => '', 'desc' => '', 'keywords' => ''];
 
-    public function __construct($route,$layout = '',$view = ''){
+    public function __construct($route, $layout = '', $view = '')
+    {
         $this->route = $route;
-        if($layout === false){
+        if ($layout === false) {
             $this->layout = false;
-        }else{
-        $this->layout = $layout ?: LAYOUT;
+        } else {
+            $this->layout = $layout ?: LAYOUT;
         }
         $this->view = $view;
     }
 
-    public function render($vars = []){
-        $this->route['prefix'] = str_replace('\\','/',$this->route['prefix']);
-        if(is_array($vars)){
-           // debug($vars);
+    public function render($vars = [])
+    {
+        $this->route['prefix'] = str_replace('\\', '/', $this->route['prefix']);
+        if (is_array($vars)) {
+            // debug($vars);
             extract($vars);
         }
-         
+
         $file_view = APP . "/views/{$this->route['prefix']}{$this->route['controller']}/{$this->view}.php";
-        ob_start([$this,'compressPage']);
-        if(is_file($file_view)){
-            require $file_view;
-        }else{
-            throw new \Exception("<br>Not found view <b>$file_view</b>",404);
+        ob_start([$this, 'compressPage']); 
+        {
+            if (is_file($file_view)) {
+                require $file_view;
+            } else {
+                throw new \Exception("<br>Not found view <b>$file_view</b>", 404);
+            }
+            $content = ob_get_contents();
         }
-        $content = ob_get_contents();
         ob_clean();
         //$content = ob_get_clean();
 
-        if($this->layout !== false){
-            $file_layout = APP."/views/layouts/{$this->layout}.php";
-            if(is_file($file_layout))
-            {
+        if ($this->layout !== false) {
+            $file_layout = APP . "/views/layouts/{$this->layout}.php";
+            if (is_file($file_layout)) {
                 $content = $this->getScripts($content);
                 $scripts = [];
-                if(!empty($this->scripts[0]))
-                   {
+                if (!empty($this->scripts[0])) {
                     $scripts = $this->scripts[0];
-                   }
+                }
                 require $file_layout;
-            }else{
-                throw new \Exception( "<br>Not Found layout $file_layout",404);
+            } else {
+                throw new \Exception("<br>Not Found layout $file_layout", 404);
             }
         }
     }
 
-    protected function compressPage($buffer){
+    protected function compressPage($buffer)
+    {
         $search = [
             "/(\n)+/",
             "/\r\n+/",
@@ -86,28 +91,30 @@ class View{
             "><"
         ];
 
-        return preg_replace($search,$replace,$buffer);
+        return preg_replace($search, $replace, $buffer);
     }
 
-    protected function getScripts($content){
+    protected function getScripts($content)
+    {
         $pattern = "#<script.*?>.*?</script>#si";
-        preg_match_all($pattern,$content,$this->scripts);
-        if(!empty($this->scripts)){
-            $content = preg_replace($pattern,'',$content);
+        preg_match_all($pattern, $content, $this->scripts);
+        if (!empty($this->scripts)) {
+            $content = preg_replace($pattern, '', $content);
         }
         return $content;
     }
 
-    public static function getMeta(){
-        echo '<title>'.self::$meta['title'].'</title>
-        <meta name="description" content="' .self::$meta['desc']. '">
-        <meta name="keywords" content="' .self::$meta['keywords']. '">';
+    public static function getMeta()
+    {
+        echo '<title>' . self::$meta['title'] . '</title>
+        <meta name="description" content="' . self::$meta['desc'] . '">
+        <meta name="keywords" content="' . self::$meta['keywords'] . '">';
     }
 
-    public static function setMeta($title = '',$desc='',$keywords=''){
+    public static function setMeta($title = '', $desc = '', $keywords = '')
+    {
         self::$meta['title'] = $title;
         self::$meta['desc'] = $desc;
         self::$meta['keywords'] = $keywords;
     }
-
 }
