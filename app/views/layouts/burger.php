b71cafc42ce018ba74285f15d1796d444bd12154
Sara894 <t48250klo@gmail.com>
2022-05-09 13:01:12 +0300

    чувашский и казахский языки, проверка шрифтов

--- app/views/layouts/burger.php
+++ app/views/layouts/burger.php
@@ -134,9 +134,10 @@ use fw\core\base\View;
                 <div class="col-xl-5 col-lg-5 offset-lg-1 col-md-6">
                     <div class="about_info">
                         <div class="section_title mb-20px">
-                            <span>About Us</span>
-                            <h3>Best Burger <br>
-                                    in your City</h3>
+                            <span>Біз туралы</span>
+                            <h3>Сіздің қалаңыздағы ең  <br>
+                            жақсы Бургерлер</h3>
+                            чи лайӑх бургер
                         </div>
                         <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate</p>
                         <div class="img_thumb">
