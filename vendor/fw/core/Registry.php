3c68c9e6ee26c253be510399bce9103b9ab48ab7
Sara894 <t48250klo@gmail.com>
2022-05-09 13:16:06 +0300

    переписываем класс Registry

--- vendor/fw/core/Registry.php
+++ vendor/fw/core/Registry.php
@@ -6,55 +6,20 @@ class Registry
 {
     use TSingletone;
 
-    public static $objects = [];
+    protected static $properties = [];
 
-    protected static $instance;
+    public function setProperty($name,$value){
+        self::$properties[$name] = $value; 
+    }
 
-    protected function __construct()
-    {
-        require '../config/config.php';
-        foreach ($config['components'] as $name => $component) {
-            /*    echo $component;
-        echo $name; */
-            self::$objects[$name] = new $component;
+    public function getProperty($name){
+        if(isset(self::$properties[$name])){
+            return self::$properties[$name];
         }
+        return null;
     }
 
-    /* public static function instance(){
-    if(self::$instance == null){
-        self::$instance = new self;
-    }
-  //  var_dump(self::$instance->getList());
-    return self::$instance;
-} */
-
-    /**
-     * данный методы - магический, он вызывается при 
-     * вызове несуществующего метода
-     */
-    public function __call($nameOfMethod, $argumentsOfMethod)
-    {
-        echo '</br>' . $nameOfMethod . " not exists";
-        var_dump($argumentsOfMethod);
-    }
-    public function getList()
-    {
-        echo '<pre>';
-        var_dump(self::$objects);
-        echo '</pre>';
-    }
-
-    public function __get($name)
-    {
-        if (is_object(self::$objects[$name])) {
-            return self::$objects[$name];
-        }
-    }
-
-    public function __set($name, $object)
-    {
-        if (!isset(self::$objects[$name])) {
-            self::$objects[$name] = new $object;
-        }
+    public function getProperties(){
+        return self::$properties;
     }
 }
