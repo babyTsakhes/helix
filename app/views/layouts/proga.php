f7cd9b23daae1fb19ce418e46fd03a2226dc9170
Sara894 <t48250klo@gmail.com>
2022-05-09 19:14:25 +0300

    настройка сервера для доступа к скриптам

--- app/views/layouts/proga.php
+++ app/views/layouts/proga.php
@@ -160,6 +160,7 @@
 <footer class="w3-center w3-black w3-padding-16">
   <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-text-green">w3.css</a></p>
 </footer>
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src = "/proga/js/main.js"> </script>
 </body>
 </html>
