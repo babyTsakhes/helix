88c6d10a6c20e107c86e4d65cd4ef1581ab39242
Sara894 <t48250klo@gmail.com>
2022-05-09 08:54:26 +0300

    включаемая область с галереей бургеров

--- app/views/inc/sidebar.php
+++ app/views/inc/sidebar.php
@@ -1,1 +1,49 @@
-TEST
+  <div class="gallery_area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12">
+                            <div class="section_title mb-70 text-center">
+                                    <span>Gallery Image</span>
+                                    <h3>Our Gallery</h3>
+                                </div>
+                    </div>
+                </div>
+            </div>
+            <div class="single_gallery big_img">
+                    <a class="popup-image" href="/burger/img/gallery/6.png">
+                        <i class="ti-plus"></i>
+                    </a>
+                <img src="/burger/img/gallery/6.png" alt="">
+            </div>
+            <div class="single_gallery small_img">
+                <a class="popup-image" href="/burger/img/gallery/6.png">
+                    <i class="ti-plus"></i>
+                </a>
+                <img src="/burger/img/gallery/6.png" alt="">
+            </div>
+            <div class="single_gallery small_img">
+                <a class="popup-image" href="/burger/img/gallery/6.png">
+                    <i class="ti-plus"></i>
+                </a>
+                <img src="/burger/img/gallery/6.png" alt="">
+            </div>
+    
+            <div class="single_gallery small_img">
+                <a class="popup-image" href="/burger/img/gallery/6.png">
+                    <i class="ti-plus"></i>
+                </a>
+                <img src="/burger/img/gallery/6.png" alt="">
+            </div>
+            <div class="single_gallery small_img">
+                <a class="popup-image" href="/burger/img/gallery/6.png">
+                    <i class="ti-plus"></i>
+                </a>
+                <img src="/burger/img/gallery/6.png" alt="">
+            </div>
+            <div class="single_gallery big_img">
+                <a class="popup-image" href="/burger/img/gallery/6.png">
+                    <i class="ti-plus"></i>
+                </a>
+                <img src="/burger/img/gallery/6.png" alt="">
+            </div>
+        </div>
--- app/views/layouts/burger.php
+++ app/views/layouts/burger.php
@@ -150,55 +150,7 @@ use fw\core\base\View;
     <!-- about_area_end -->
 
     <!-- gallery_start -->
-   <!--  <div class="gallery_area">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-12">
-                            <div class="section_title mb-70 text-center">
-                                    <span>Gallery Image</span>
-                                    <h3>Our Gallery</h3>
-                                </div>
-                    </div>
-                </div>
-            </div>
-            <div class="single_gallery big_img">
-                    <a class="popup-image" href="/burger/img/gallery/1.png">
-                        <i class="ti-plus"></i>
-                    </a>
-                <img src="/burger/img/gallery/1.png" alt="">
-            </div>
-            <div class="single_gallery small_img">
-                <a class="popup-image" href="/burger/img/gallery/2.png">
-                    <i class="ti-plus"></i>
-                </a>
-                <img src="/burger/img/gallery/2.png" alt="">
-            </div>
-            <div class="single_gallery small_img">
-                <a class="popup-image" href="/burger/img/gallery/3.png">
-                    <i class="ti-plus"></i>
-                </a>
-                <img src="/burger/img/gallery/3.png" alt="">
-            </div>
-    
-            <div class="single_gallery small_img">
-                <a class="popup-image" href="/burger/img/gallery/4.png">
-                    <i class="ti-plus"></i>
-                </a>
-                <img src="/burger/img/gallery/4.png" alt="">
-            </div>
-            <div class="single_gallery small_img">
-                <a class="popup-image" href="/burger/img/gallery/5.png">
-                    <i class="ti-plus"></i>
-                </a>
-                <img src="/burger/img/gallery/5.png" alt="">
-            </div>
-            <div class="single_gallery big_img">
-                <a class="popup-image" href="/burger/img/gallery/6.png">
-                    <i class="ti-plus"></i>
-                </a>
-                <img src="/burger/img/gallery/6.png" alt="">
-            </div>
-        </div> -->
+  
 <?$this->getPart('inc/sidebar');?>
     <!-- testimonial_area_start  -->
     <div class="testimonial_area ">
