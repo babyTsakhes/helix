57ae7718aa7e7a3b93db83fce7e85d960bfa8576
Sara894 <t48250klo@gmail.com>
2022-05-10 13:01:05 +0300

    вывод перевода в шаблоне

--- app/langs/en.php
+++ app/langs/en.php
@@ -12,4 +12,5 @@ return [
     'send message'=>'send message',
     'Architect'=>'Architect',
     'CEO & Founder'=>'CEO & Founder',
+    'big text'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
 ];
--- vendor/fw/libs/functions.php
+++ vendor/fw/libs/functions.php
@@ -19,3 +19,7 @@ function redirect($http = false){
 function h($str){
     return htmlspecialchars($str,ENT_QUOTES);
 }
+
+function __($key){
+    echo fw\core\base\Lang::get($key);
+}
