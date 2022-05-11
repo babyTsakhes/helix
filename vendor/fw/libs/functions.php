c7e64c9a0a17fa571bfcbbdf630425cff5ac5ddf
Sara894 <t48250klo@gmail.com>
2022-05-11 21:22:04 +0300

    debug redaction

--- vendor/fw/libs/functions.php
+++ vendor/fw/libs/functions.php
@@ -1,9 +1,11 @@
 <?php
 
-function debug($arr){
+function debug($arr, $die = false){
     echo "<pre>";
     var_dump($arr);
     echo '</pre>';
+    if($die)
+        die;
 }
 
 function redirect($http = false){
